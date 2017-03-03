63b053fc22d95e4885941e45dbe384c8187aeec4
M.C. van den Boogaart <scola@kvdb.name>
2017-03-03 21:26:20 +0100

    Refactor exports

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-export { default as PubSub } from './src/pubsub';
-export { default as publish } from './src/helper/publish';
-export { default as subscribe } from './src/helper/subscribe-list';
-export { default as subscribeList } from './src/helper/subscribe-list';
-export { default as subscribeObject } from './src/helper/subscribe-object';
+import PubSub from './src/pubsub';
+import publish from './src/helper/publish';
+import subscribe from './src/helper/subscribe-list';
+import subscribeList from './src/helper/subscribe-list';
+import subscribeObject from './src/helper/subscribe-object';
+
+export {
+  PubSub,
+  publish,
+  subscribe,
+  subscribeList,
+  subscribeObject
+};
